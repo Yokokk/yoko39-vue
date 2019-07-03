8b915b9cbaa08360e923420b909010d0cdbffed9
zhangyuke <yoko.zyk.sh@gmail.com>
2019-07-03 11:52:47 +0800

    登录功能基本实现

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,23 @@
 <template>
   <el-row type="flex" class="row-bg" justify="center" align="middle">
     <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
-      <el-form ref="form" :model="form" label-position="top" label-width="80px" class="login-form">
-        <el-form-item label="用户名" required="true">
+      <el-form
+        ref="loginForm"
+        :rules="formRules"
+        :model="form"
+        label-position="top"
+        label-width="80px"
+        class="login-form"
+      >
+        <el-form-item label="用户名" prop="username">
           <el-input v-model="form.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码" required="true">
-          <el-input v-model="form.passward"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
         </el-form-item>
-
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -19,18 +25,65 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       form: {
-        username: "",
-        passward: ""
+        username: "admin",
+        password: "123456"
+      },
+      formRules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          },
+          {
+            min: 5,
+            max: 12,
+            message: "用户名必须要5-12个字符"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          {
+            min: 4,
+            max: 16,
+            message: "密码必须要4-16个字符"
+          }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: this.form
+          }).then(res => {
+            // console.log(res);
+            if (res.data.meta.status == 200) {
+              // console.log(res.data.meta.msg);
+              this.$router.push("/home");
+            }
+          });
+        } else {
+          // console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
